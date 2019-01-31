e03d1622cbe5570a874d2227d225f23333364a4e
Jack Steffens <jack.steffens@trifork.nl>
2019-01-31 11:15:08 +0100

    Removed AngularJS files

--- public/components/player-keyboard/player-keyboard.directive.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('WebMIDI').directive('playerKeyboard', function () {
-    return {
-        restrict: 'E',
-        controller: 'PlayerKeyboardCtrl',
-        templateUrl: 'components/player-keyboard/player-keyboard.html'
-    }
-});
